e72fc681616a4a340ead4aa9be5437f9bf690c1b
Wesley Persad <26929215+wesleypersad@users.noreply.github.com>
2023-02-13 19:10:14 +0000

    Extra routes added to charity route for research etc.

--- routes/charity.js
+++ routes/charity.js
@@ -9,9 +9,54 @@ require('dotenv').config();
 const charitiesAPIKey = process.env.CHARITIES_API_KEY;
 const charitiesAPIURI = process.env.CHARITIES_API_URI;
 
-router.get('/', async (req, res) => {
+// just for the charity branch
+router.get('/', (req, res, next) => { 
+    res.send('CHARITY GENERAL PATH !!!');
+})
+
+router.get('/details/:charnum', async (req, res) => {
     try {
-        const response = await fetch(`${charitiesAPIURI}/charitydetails/214779/0`, {
+        console.log(req.params.charnum);
+        const charnum = req.params.charnum;
+        const response = await fetch(`${charitiesAPIURI}/charitydetails/${charnum}/0`, {
+            method: 'GET',
+            // Request headers
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Ocp-Apim-Subscription-Key': `${charitiesAPIKey}`
+            }
+        });
+        res.json(await response.json());
+    } catch(error) {
+        console.log(error.message);
+        return null;
+    };
+});
+
+router.get('/searchname/:name', async (req, res) => {
+    try {
+        console.log(req.params.name);
+        const name = req.params.name;
+        const response = await fetch(`${charitiesAPIURI}/searchCharityName/${name}`, {
+            method: 'GET',
+            // Request headers
+            headers: {
+                'Cache-Control': 'no-cache',
+                'Ocp-Apim-Subscription-Key': `${charitiesAPIKey}`
+            }
+        });
+        res.json(await response.json());
+    } catch(error) {
+        console.log(error.message);
+        return null;
+    };
+});
+
+router.get('/financialhistory/:charnum', async (req, res) => {
+    try {
+        console.log(req.params.charnum);
+        const charnum = req.params.charnum;
+        const response = await fetch(`${charitiesAPIURI}/charityfinancialhistory/${charnum}/0`, {
             method: 'GET',
             // Request headers
             headers: {
